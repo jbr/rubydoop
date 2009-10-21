646c4108aa73f6d3a93f26397519aa4557dda087
Jacob Rothstein <github@jacobrothstein.com>
2009-10-20 22:47:02 -0700

    initial extraction

---
+++ rubydoop.rb
@@ -0,0 +1,52 @@
+def map(&proc)
+  @map = proc
+end
+
+def reduce(&proc)
+  @reduce = proc
+end
+
+at_exit do
+  case ARGV.first
+  when 'start'
+    cmd = <<-EOC
+      hadoop fs -rmr output
+      hadoop jar /usr/local/hadoop/contrib/streaming/hadoop-*-streaming.jar\\
+        -inputformat org.apache.hadoop.mapred.KeyValueTextInputFormat\\
+        -output output -input input\\
+        -file #{File.expand_path __FILE__} \\
+        -file #{File.expand_path $0} \\
+        -mapper "#{File.basename $0} map" \\
+        -reducer "#{File.basename $0} reduce"
+    EOC
+    puts cmd
+    exec cmd
+  when 'map'
+    while line = STDIN.gets
+      if line =~ /^([^\t]+)\t(.+)$/
+        @map.call $1, $2
+      end
+    end
+  when 'reduce'
+    key, values = nil, []
+    while line = STDIN.gets
+      if line =~ /^([^\t]+)\t(.+)$/
+        thiskey, thisvalue = $1, $2
+        if key != thiskey && key
+          @reduce.call key, values
+          key, values = nil, []
+        end
+        key = thiskey
+        values << thisvalue
+      end
+    end
+  else
+    STDERR.puts <<-EOM
+Please run "#{$0} COMMAND", where COMMAND is one of the following:
+\tstart
+\tmap
+\treduce
+EOM
+    exit -1
+  end
+end
